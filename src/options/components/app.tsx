2123a361e09bf644f61aa8d1dee52b1b88d7f0b8
weldar0616 <weldar0509@gmail.com>
2021-10-02 21:51:14 +0900

    APIトークン入力フィールドをコンポーネント化(#4)

--- src/options/components/app.tsx
+++ src/options/components/app.tsx
@@ -1,20 +1,34 @@
 import React from 'react';
-import { saveChromeStorage } from '../../common/core';
-import { TOGGL_REPORTS_API_TOKEN_KEY } from '../../common/const';
 import { Button } from '@mui/material';
+import { PasswordInput } from './passwordInput';
+import { TOGGL_REPORTS_API_TOKEN_KEY } from '../../common/const';
+import { saveChromeStorage } from '../../common/core';
+
+interface State {
+  apiToken: string;
+}
 
 export const App = () => {
+  const [userSettings, setUserSettings] = React.useState<State>({
+    apiToken: '',
+  });
+
   const handleOnClick = () => {
-    const elm: HTMLInputElement = document.getElementById(
-      'input-api-token',
-    ) as HTMLInputElement;
-    const inputValue = elm?.value.trim();
-    saveChromeStorage(TOGGL_REPORTS_API_TOKEN_KEY, inputValue);
+    saveChromeStorage(TOGGL_REPORTS_API_TOKEN_KEY, userSettings.apiToken);
+  };
+
+  const setApiToken = (value: string) => {
+    setUserSettings({ apiToken: value });
   };
 
   return (
     <div className="App">
-      <input type="text" id="input-api-token"></input>
+      <PasswordInput
+        label="API Token"
+        edittingValue={userSettings.apiToken}
+        onEdittingValueUpdated={setApiToken}
+      />
+      {/* TODO: Buttonの配置を右端に */}
       <Button variant="outlined" onClick={handleOnClick}>
         保存
       </Button>
