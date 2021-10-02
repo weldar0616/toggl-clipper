06a067aaa0bceda2b1ce09c1f71b891edcd9e56f
weldar0616 <weldar0509@gmail.com>
2021-10-02 17:20:25 +0900

    tsファイル名をキャメルケースに変更(#23)

--- src/popup/components/popupButton.tsx
+++ src/popup/components/popupButton.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import { clipper } from '../../common/clipper';
-import { togglTask } from '../../common/toggl-task';
+import { togglTask } from '../../common/togglTask';
 
 export const PopupButton = () => {
   const handleOnClick = () => {
