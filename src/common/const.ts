ff358225d424fd03d6fb6fa0c14e6543a29309e0
weldar0616 <weldar0509@gmail.com>
2021-10-02 22:47:28 +0900

    togglAPI: workspace_idをoptionsで登録できるように(#24)

--- src/common/const.ts
+++ src/common/const.ts
@@ -1,1 +1,2 @@
 export const TOGGL_REPORTS_API_TOKEN_KEY: string = 'toggl_reports_api_token';
+export const TOGGL_REPORTS_WORKSPACE_ID: string = 'toggl_reports_workspace_id';
--- src/options/components/app.tsx
+++ src/options/components/app.tsx
@@ -1,34 +1,61 @@
 import React from 'react';
 import { Button } from '@mui/material';
 import { PasswordInput } from './passwordInput';
-import { TOGGL_REPORTS_API_TOKEN_KEY } from '../../common/const';
+import {
+  TOGGL_REPORTS_API_TOKEN_KEY,
+  TOGGL_REPORTS_WORKSPACE_ID,
+} from '../../common/const';
 import { loadChromeStorage, saveChromeStorage } from '../../common/core';
 
 interface State {
+  workspaceId: string;
   apiToken: string;
 }
 
+const SETTINGS_LABEL = {
+  workspaceId: 'workspaceId',
+  apiToken: 'apiToken',
+};
+
 export const App = () => {
   const [userSettings, setUserSettings] = React.useState<State>({
+    workspaceId: '',
     apiToken: '',
   });
 
   React.useEffect(() => {
-    loadChromeStorage(TOGGL_REPORTS_API_TOKEN_KEY).then(setApiToken);
-  });
+    loadChromeStorage(TOGGL_REPORTS_WORKSPACE_ID).then((response) => {
+      if (response) setWorkspaceId(response);
+    });
+    loadChromeStorage(TOGGL_REPORTS_API_TOKEN_KEY).then((response) => {
+      if (response) setApiToken(response);
+    });
+  }, []);
 
   const handleOnClick = () => {
+    saveChromeStorage(TOGGL_REPORTS_WORKSPACE_ID, userSettings.workspaceId);
     saveChromeStorage(TOGGL_REPORTS_API_TOKEN_KEY, userSettings.apiToken);
   };
 
+  const updateUserSettings = (value: string, prop: string) => {
+    setUserSettings({ ...userSettings, [prop]: value });
+  };
+  const setWorkspaceId = (value: string) => {
+    updateUserSettings(value, SETTINGS_LABEL.workspaceId);
+  };
   const setApiToken = (value: string) => {
-    setUserSettings({ apiToken: value });
+    updateUserSettings(value, SETTINGS_LABEL.apiToken);
   };
 
   return (
     <div className="App">
       <PasswordInput
-        label="API Token"
+        label={SETTINGS_LABEL.workspaceId}
+        edittingValue={userSettings.workspaceId}
+        onEdittingValueUpdated={setWorkspaceId}
+      />
+      <PasswordInput
+        label={SETTINGS_LABEL.apiToken}
         edittingValue={userSettings.apiToken}
         onEdittingValueUpdated={setApiToken}
       />
